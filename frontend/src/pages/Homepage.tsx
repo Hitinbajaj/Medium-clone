8c3b5a909eff65e61da00bcfa27800b2697d9724
Hitinbajaj <hitinbajaj77@gmail.com>
2024-03-09 19:07:14 +0530

    home page added

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -7,12 +7,12 @@ export const Homepage = () => {
       <div className="flex">
         <div className="text-slate-500">
           <Link className="w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" to={"/signin"}>
-            Sign up
+            Sign in
           </Link>
         </div>
         <div className="text-slate-500">
           <Link className="w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" to={"/signup"}>
-            Sign in
+            Sign up
           </Link>
         </div>
       </div>
